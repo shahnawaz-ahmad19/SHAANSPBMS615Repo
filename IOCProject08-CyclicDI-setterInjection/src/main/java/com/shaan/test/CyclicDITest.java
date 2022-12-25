acc10e19c4261d1be256843baef415a298697289
Shahnawaz <Shahnawaz@SHAAN>
2022-12-25 18:19:30 +0530

    CyclicDI-SetterInjection..

---
+++ IOCProject08-CyclicDI-setterInjection/src/main/java/com/shaan/test/CyclicDITest.java
@@ -0,0 +1,21 @@
+package com.shaan.test;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
+import com.shaan.beans.A;
+
+public class CyclicDITest {
+	public static void main(String[] args) {
+		//Create IOC container
+		DefaultListableBeanFactory factory=new DefaultListableBeanFactory();
+		XmlBeanDefinitionReader reader=new XmlBeanDefinitionReader(factory);
+		reader.loadBeanDefinitions("com/shaan/cfgs/applicationContext.xml");
+		
+		//get Spring bean class obj
+		A oa=factory.getBean("a1", A.class);
+		System.out.println(oa);
+	}
+
+}
+ 
